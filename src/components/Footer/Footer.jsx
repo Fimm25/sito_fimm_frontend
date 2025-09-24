0d8165118e544fc3e0cb7f7fc5a7e9270a57e319
Lellefrez <manuel.frezzato@gmail.com>
2025-09-24 17:22:58 +0200

    rimossa una |

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -30,10 +30,10 @@ export default () => {
   <a href="../../public/SA8000.pdf" target="_blank" rel="noopener noreferrer">
     Politica della Responsabilità Sociale
   </a>
-  <p>|</p>|
-  <a href="../../public/PoliticaInformazioni.pdf" target="_blank" rel="noopener noreferrer">Politica per la Sicurezza delle Informazioni</a>
   
 </p>
+ <p>|</p>
+  <a href="../../public/PoliticaInformazioni.pdf" target="_blank" rel="noopener noreferrer">Politica per la Sicurezza delle Informazioni</a>
 <p>
   <a href="../../public/Segnalazioni.pdf" target="_blank" rel="noopener noreferrer">
     Modalità di segnalazione relative alla Responsabilità Sociale
