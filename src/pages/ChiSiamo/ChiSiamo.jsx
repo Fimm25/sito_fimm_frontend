c32c1755ba71ff04c41fcbe4af212aa126846e35
Lellefrez <manuel.frezzato@gmail.com>
2025-10-24 09:20:47 +0200

    fix| modifiche certificazioni iso

--- src/pages/ChiSiamo/ChiSiamo.jsx
+++ src/pages/ChiSiamo/ChiSiamo.jsx
@@ -1,9 +1,6 @@
 import { useEffect, useState, useRef } from "react"; // Aggiungi useEffect
 import "./ChiSiamo.scss";
-import Sa800 from "../../assets/images/certificazioni/download.png";
-import Iso9001 from "../../assets/images/certificazioni/ISO-9001.png";
-import Iso14000 from "../../assets/images/certificazioni/iso-14000.png";
-import Iso45001 from "../../assets/images/certificazioni/46.png";
+import { FaFilePdf } from "react-icons/fa";
 import sede_azienda from "../../assets/images/f.imm/sede_fimm.jpg";
 import videoReal from "../../assets/videos/videoRealTagliato.mp4" // video
 import fotoReal from "../../assets/images/cover-img/coverVideoTagliato.png" // cover video
@@ -131,45 +128,62 @@ const ChiSiamo = () => {
         <h2>F.IMM negli anni</h2>
         <hr />
       </div>
+
       <section>
         <div className="timeline">
           <CustomizedTimeline />
         </div>
       </section>
+
       <section className="certified-section" id="certificazioni">
         <div className="divider">
           <h2>Un'azienda certificata</h2>
           <hr />
         </div>
+
         <div className="grid-container">
           {[
             {
               title: "SA 8000",
               description:
                 "Lo standard accreditato riconosciuto a livello internazionale che risponde alle esigenze delle organizzazioni che vogliono distinguersi per il loro impegno nello sviluppo sostenibile e in particolare per le tematiche sociali.",
-            //  img: Sa800,
+              pdf: "/certification/SA8000.pdf",
             },
             {
               title: "ISO 9001",
               description:
                 "Definisce i requisiti minimi che il Sistema di Gestione per la Qualità di un'organizzazione deve dimostrare di soddisfare per garantire il livello di qualità di prodotto e servizio.",
-             // img: Iso9001,
+              pdf: "/certification/ISO9001.pdf",
             },
             {
               title: "ISO 45001",
               description:
                 "Lo standard internazionale per la salute e sicurezza sul lavoro, progettato per proteggere dipendenti e visitatori da incidenti e malattie legate al lavoro.",
-             // img: Iso45001,
+              pdf: "/certification/ISO45001.pdf",
             },
             {
-              title: "ISO 14000",
+              title: "ISO 14001",
               description:
                 "Identifica una serie di norme tecniche relative alla gestione ambientale delle organizzazioni.",
-             // img: Iso14000,
+              pdf: "/certification/ISO14001.pdf",
+            },
+            {title: "ISO 27001",
+            description:
+              "Attesta la conformità e l'efficacia di un sistema di gestione per la sicurezza delle informazioni, garantendo la protezione di dati e informazioni.",
+            pdf: "/certification/ISO27001.pdf",
             },
           ].map((item, index) => (
             <div className="grid-item" key={index}>
-              {/*<img src={item.img} alt={item.title} />*/}
+              <a
+                href={item.pdf}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="pdf-icon"
+                title={`Apri certificazione ${item.title}`}
+              >
+                <FaFilePdf size={50} color="#d32f2f" />
+              </a>
+
               <div className="text-content">
                 <h5>{item.title}</h5>
                 <p>{item.description}</p>
